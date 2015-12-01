29990430593439eafebfbe3c697433dd5602a333
HenrikObsen <hto@djes.dk>
2015-12-01 10:47:44 +0100

    En hel masse

--- RepoBlog/Factories/PostFac.cs
+++ RepoBlog/Factories/PostFac.cs
@@ -4,10 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RepoBlog.Models.BaseModels;
+using RepoBlog.Models.ViewModels;
 
 namespace RepoBlog.Factories
 {
     public class PostFac: AutoFac<Post>
     {
+        BilledFac billedFac = new BilledFac();
+
+        public List<BlogIndex> GetIndexData()
+        {
+            List<BlogIndex> listBlogIndex = new List<BlogIndex>();
+            foreach (var post in GetAll())
+            {
+                BlogIndex blogIndex = new BlogIndex();
+                blogIndex.Post = post;
+                blogIndex.Billeder = billedFac.GetBy("PostID", post.ID);
+                listBlogIndex.Add(blogIndex);
+            }
+            return listBlogIndex;
+        }
     }
 }
