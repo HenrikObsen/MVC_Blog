5291e3c6ca1cada5f63f4a5a1f7f3caed30d395c
HenrikObsen <hto@djes.dk>
2015-12-03 10:12:06 +0100

    tt

--- MVC_Blog/Controllers/BlogController.cs
+++ MVC_Blog/Controllers/BlogController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
 using RepoBlog.Factories;
 using RepoBlog.Models.BaseModels;
+using System.IO;
 
 namespace MVC_Blog.Controllers
 {
@@ -15,6 +17,13 @@ namespace MVC_Blog.Controllers
             return View(postFac.GetIndexData());
         }
 
+        public ActionResult Delete(int id)
+        {
+            postFac.Delete(id);
+            return View("index", postFac.GetIndexData());
+        }
+
+
 
         public ActionResult Edit(int id)
         {
@@ -22,6 +31,7 @@ namespace MVC_Blog.Controllers
         }
 
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Edit(Post p)
         {
             p.Dato = DateTime.Now;
@@ -30,8 +40,58 @@ namespace MVC_Blog.Controllers
             {
                 postFac.Update(p);
             }
-            
+
             return View(postFac.Get(p.ID));
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Post p)
+        {
+            p.Dato = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                postFac.Insert(p);
+                ViewBag.MSG = "Posten er oprettet!";
+            }
+            else
+            {
+                ViewBag.MSG = "Der opstod en fejl!";
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Upload(int id)
+        {
+            return View(id);
+        }
+
+        [HttpPost]
+        public ActionResult Upload(int id, HttpPostedFileBase fil)
+        {
+            if (fil != null)
+            {
+                Uploader u = new Uploader();
+                string path = Request.PhysicalApplicationPath + "Content/Images/";
+                string file = u.UploadImage(fil, path, 300, true);
+
+                BilledFac bf = new BilledFac();
+                Billede b = new Billede();
+                b.Filnavn = Path.GetFileName(file);
+                b.PostID = id;
+                bf.Insert(b);
+
+            }
+
+            return View("Index", postFac.GetIndexData());
         }
     }
 }
---
+++ MVC_Blog/Helpers/Uploader.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+
+//Upload af store filer. Indsættes i web.config:<httpRuntime maxRequestLength="2097151" executionTimeout="1200" />
+
+
+    public class Uploader
+    {
+        private Random rnd = new Random();
+
+        public string UploadFile(HttpPostedFile fileToUpload, string saveToPath, bool randomFileName,
+            string allowExtentons)
+        {
+            string strFileName;
+            string strLongFilePath = fileToUpload.FileName;
+            bool done = false;
+            strFileName = Path.GetFileName(strLongFilePath);
+
+
+            if (allowExtentons.Length > 1)
+            {
+                string extentions = allowExtentons.Replace(" ", "").ToLower();
+                extentions = allowExtentons.Replace(".", "");
+                string[] arrExt = extentions.Split(',');
+
+                foreach (string ext in arrExt)
+                {
+                    if ("." + ext == Path.GetExtension(strFileName))
+                    {
+                        if (randomFileName)
+                        {
+                            strFileName = GenRndName() + Path.GetExtension(strFileName);
+
+                        }
+                        else
+                        {
+                            strFileName = ReplaceChars(strFileName);
+                        }
+
+                        fileToUpload.SaveAs(saveToPath + strFileName);
+
+                        done = true;
+                    }
+
+                }
+
+            }
+            else
+            {
+                if (randomFileName)
+                {
+                    strFileName = GenRndName() + Path.GetExtension(strFileName);
+                }
+                else
+                {
+                    strFileName = ReplaceChars(strFileName);
+                }
+                fileToUpload.SaveAs(saveToPath + strFileName);
+                done = true;
+            }
+
+            if (done)
+            {
+                return saveToPath + strFileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public string UploadImage(HttpPostedFileBase fileToUpload, string outputPath, int newWidth, bool randomFileName)
+        {
+            string strFileName;
+            string strLongFilePath = fileToUpload.FileName;
+            string ext = Path.GetExtension(strLongFilePath).ToLower();
+            var imgFor = GetFormat(ext);
+
+            if (imgFor != null)
+            {
+
+                strFileName = Path.GetFileName(strLongFilePath);
+
+                if (randomFileName)
+                {
+                    strFileName = GenRndName() + Path.GetExtension(strFileName);
+                }
+                else
+                {
+                    strFileName = ReplaceChars(strFileName);
+                }
+
+
+                fileToUpload.SaveAs(outputPath + strFileName);
+
+                if (newWidth > 0)
+                {
+                    this.ReSizeImage(outputPath + strFileName, outputPath, newWidth);
+                }
+
+                else
+                {
+                    return outputPath + strFileName;
+                }
+                return outputPath + strFileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string ReSizeImage(string imagePath, string outputPath, int newWidth)
+        {
+            System.Drawing.Image bm = System.Drawing.Image.FromFile(imagePath);
+            string ext = Path.GetExtension(imagePath);
+            string fileN = Path.GetFileName(imagePath);
+            var imgFor = GetFormat(ext);
+            int NewHeight = (bm.Height*newWidth)/bm.Width;
+
+            Bitmap resized = new Bitmap(newWidth, NewHeight);
+
+            Graphics g = Graphics.FromImage(resized);
+
+            g.DrawImage(bm, new Rectangle(0, 0, resized.Width, resized.Height), 0, 0, bm.Width, bm.Height,
+                GraphicsUnit.Pixel);
+
+            g.Dispose();
+            bm.Dispose();
+
+            if (imgFor != null)
+            {
+                resized.Save(outputPath + fileN, imgFor);
+                return outputPath + fileN;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private string ReplaceChars(string text)
+        {
+            text = text.Replace(" ", "_");
+            text = text.Replace("æ", "ae");
+            text = text.Replace("ø", "oe");
+            text = text.Replace("å", "aa");
+            text = text.Replace(",", "");
+            text = text.Replace("*", "");
+            text = text.Replace("'", "");
+            text = text.Replace("&", "");
+
+            return text;
+        }
+
+
+        public string GenRnd(int antal)
+        {
+
+            string strKey =
+                "a b c d e f g h i j k l m n o p q r s t u v x y z 1 2 3 4 5 6 7 8 9 A B C D E F G H I J K L M N O P Q R S T U V X Y Z";
+
+            string[] arrKey = {};
+            string rndString = "";
+
+            arrKey = strKey.Split(' ');
+
+            int intMax = arrKey.Length;
+            int intMin = 1;
+
+            for (int i = 0; i < antal; i++)
+            {
+                rndString += arrKey[rnd.Next(intMin, intMax)];
+            }
+
+            return rndString;
+        }
+
+        private string GenRndName()
+        {
+            DateTime d = DateTime.Now;
+            string name = d.Day + GenRnd(7) + d.Year + GenRnd(7) + d.Month;
+
+            return name;
+        }
+
+        private ImageFormat GetFormat(string ext)
+        {
+            switch (ext.ToLower())
+            {
+                case ".gif":
+                    return ImageFormat.Gif;
+
+                case ".jpg":
+                    return ImageFormat.Jpeg;
+
+                case ".png":
+                    return ImageFormat.Png;
+
+                default:
+                    return null;
+            }
+        }
+    }
